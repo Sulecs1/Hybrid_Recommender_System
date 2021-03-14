3ae30d51c74b659da20183b60b2de36b96ccf5d1
Sulecs1 <suleakcay96@gmail.com>
2021-03-14 22:49:06 +0300

    aynı filmlere erişmek

--- hybrid_recommender_project.py
+++ hybrid_recommender_project.py
@@ -28,3 +28,18 @@ movies_watched = random_user_df.columns[random_user_df.notna().any()].tolist()
 len(movies_watched)  #19
 
 
+#############################################
+# Adım 3: Aynı filmleri izleyen diğer kullanıcıların verisine ve id'lerine erişmek
+#############################################
+movies_watched_df = user_movie_df[movies_watched]
+movies_watched_df
+movies_watched_df.shape  #(23149, 19)
+
+user_movie_count = movies_watched_df.T.notnull().sum() #herbir kullanıcı için kaç film izlediği geldi
+user_movie_count = user_movie_count.reset_index()
+user_movie_count.columns = ["userId", "movie_count"]
+
+perc = len(movies_watched) * 60 / 100
+users_same_movies = user_movie_count[user_movie_count["movie_count"] > perc]["userId"]
+
+
