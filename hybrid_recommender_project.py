29639f93298161cf75a539356d039c41356932a0
Sulecs1 <suleakcay96@gmail.com>
2021-03-14 22:48:16 +0300

    create_user_df eklendi

--- hybrid_recommender_project.py
+++ hybrid_recommender_project.py
@@ -15,4 +15,16 @@ from helpers.helpers import create_user_movie_df
 user_movie_df = create_user_movie_df()
 user_number = 12312
 
+#############################################
+# Adım 2: Öneri yapılacak kullanıcının izlediği filmlerin belirlenmesi
+#############################################
+
+random_user_df = user_movie_df[user_movie_df.index == user_number]
+random_user_df
+
+
+movies_watched = random_user_df.columns[random_user_df.notna().any()].tolist()
+
+len(movies_watched)  #19
+
 
